14a6126d25fadc81858ba3364d1ab0f06c3363e5
Maxime <mgelinas@softframe.net>
2022-02-11 14:05:22 -0500

    chore: generate projects

---
+++ apps/todo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poc-cypress-percy-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'todo';
+}
